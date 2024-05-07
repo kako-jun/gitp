391e5c3dff0c3c318049617acf0156db22cb881b
kako-jun <3541096+kako-jun@users.noreply.github.com>
2024-05-08 00:57:25 +0900

    update.

--- gitp/src/main.rs
+++ gitp/src/main.rs
@@ -1,16 +1,24 @@
-mod git_util;
+mod git_controller;
 mod setting_util;
+use git_controller::GitController;
 // use setting_util::GitpSetting;
 
 fn main() {
+    let git_controller = GitController::new();
+    let result = git_controller.exec_command("ls", Vec::from(["-alF"]));
+    println!("{}", result);
+
     // let mut gitp_setting = GitpSetting::default();
     // gitp_setting = setting_util::load(gitp_setting).unwrap();
     let gitp_setting = setting_util::load().unwrap();
     println!("{:#?}", gitp_setting);
 
-    let result = git_util::git_clone("hoge");
+    println!("{:#?}", gitp_setting.repos[0].remote);
+    
+
+    let result = git_controller.git_clone(&gitp_setting.repos[0].remote);
     println!("{}", result);
 
-    let result = git_util::git_status();
+    let result = git_controller.git_status();
     println!("{}", result);
 }
