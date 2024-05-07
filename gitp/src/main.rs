293c35f6da81437dbe2abae03eba4943719a4430
kako-jun <3541096+kako-jun@users.noreply.github.com>
2024-05-07 20:49:33 +0900

    update.

--- gitp/src/main.rs
+++ gitp/src/main.rs
@@ -1,6 +1,13 @@
 mod git_util;
+mod setting_util;
+// use setting_util::GitpSetting;
 
 fn main() {
+    // let mut gitp_setting = GitpSetting::default();
+    // gitp_setting = setting_util::load(gitp_setting).unwrap();
+    let gitp_setting = setting_util::load().unwrap();
+    println!("{:#?}", gitp_setting);
+
     let result = git_util::git_clone("hoge");
     println!("{}", result);
 
